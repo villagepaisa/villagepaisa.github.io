d7993a300ea99a2b37d88e14a4acba5b87cd7d5f
musab17md <musab17.md@gmail.com>
2023-06-24 22:51:58 +0530

    d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -167,8 +167,8 @@ const RESOURCES = {"assets/AssetManifest.json": "7ce1fa039ba2d527721ec3bc0e2ad45
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "dddb5151e268cc074d7c3351505038b8",
-"/": "dddb5151e268cc074d7c3351505038b8",
+"index.html": "ded8337f7950756f481f2f665b5d0cfb",
+"/": "ded8337f7950756f481f2f665b5d0cfb",
 "main.dart.js": "9fb8aa4f2cf7880762fcef156beab5cd",
 "manifest.json": "3cff4400e4ea4c2e9bb290a8e6b83899",
 "version.json": "344ab2d5be59fed246fd5a68baa7b909"};
